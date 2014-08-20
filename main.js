5ada0c928a67e931337e3b9927db06d5aa68407d
Matt Lockyer <mattdlockyer@gmail.com>
2014-08-20 15:48:19 -0700

    updated file structure, further simplification of app and effects

--- main.js
+++ main.js
@@ -5,35 +5,14 @@
 var APP = APP || {};
 
 APP.init = function() {
-
-	APP.canvas = new MEDIA.Canvas();
-	var effect = new APP.RGBEffect(new MEDIA.Canvas());
-	var video = MEDIA.Camera.video;
-
-	var controls = {
-		Capture:function() {
-			APP.drawImage(APP.canvas);
-		},
-		Pixels:function() {
-			if (effect.interval) APP.Effect.stop(effect);
-			else APP.Effect.start(effect);
-		},
-		ToggleVideo:function() {
-			if (!video.style.display || video.style.display === 'none') video.style.display = 'block';
-			else video.style.display = 'none';
-		}
-	};
-
-	window.onload = function() {
-		var gui = new dat.GUI();
-		for (var key in controls) {
-			var control = controls[key];
-			switch (typeof control) {
-				case 'function': gui.add(controls, key); break;
-			}
-		}
-	};
-
+	//setup app
+	APP.setup({w:640, h:480});
+	//add effects
+	APP.effects = [];
+	APP.effects.push(new CaptureEffect(new MEDIA.Canvas()));
+	APP.effects.push(new RGBEffect(new MEDIA.Canvas()));
+	//setup controls
+	APP.setupControls();
 };
 
 APP.init();
